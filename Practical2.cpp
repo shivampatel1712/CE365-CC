bcda8d6a0f395a38bb889ff9aa69667d2822fc17
Shivam Patel <shivampatel01712@gmail.com>
2024-12-31 08:49:27 +0530

    First time

---
+++ Practical2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int numOfInputSymbol, numOfState, intialState, numOfAcceptState;
+    cout<<"Enter Number of Input Symbols : ";
+    cin>>numOfInputSymbol;
+
+    string str;
+    cout<<"Input symbols:";
+    cin>>str;
+
+    cout<<"Enter number of states: ";
+    cin>>numOfstate;
+
+    cout<<"Intial state: ";
+    cin>>intialState;
+
+    cout<<"Enter number of accepting states: ";
+    cin>>numOfAcceptState;
+
+
+
+}
